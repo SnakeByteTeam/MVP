38a51989215e63f72ed583c23e0b72dc6a3cf8bf
Filippo Venzo <venzofilippo@gmail.com>
2026-03-16 16:13:48 +0100

    aggiunto modulo auth verso vimar

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiAuthVimarModule } from './api-auth-vimar/api-auth-vimar.module';
 
 @Module({
-  imports: [],
+  imports: [ApiAuthVimarModule],
   controllers: [AppController],
   providers: [AppService],
 })

d399115b64dfee35fad5a4ebc927eeae1669c4e4
val <valeriabaleanu@gmail.com>
2026-03-22 11:43:13 +0100

    configurazione NGINX per chiamate alle analytics

--- backend/src/analytics/adapters/in/analytics.controller.ts
+++ backend/src/analytics/adapters/in/analytics.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Inject, Get, Query } from '@nestjs/common';
+import { Controller, Inject, Get, Query, Res } from '@nestjs/common';
 import { GetAnalyticsCmd } from 'src/analytics/application/commands/get-analytics.cmd';
 import { GetAnalyticsUseCase } from 'src/analytics/application/ports/in/get-analytics.usecase';
 import { GetAnalyticsDto } from 'src/analytics/infrastructure/dtos/get-analytics.dto';
 import { PlotDto } from 'src/analytics/infrastructure/dtos/plot.dto';
+import { Response } from 'express';
 
 @Controller('analytics')
 export class AnalyticsController {
@@ -12,9 +13,14 @@ export class AnalyticsController {
   ) {}
 
   @Get()
-  async getAnalytics(@Query() dto: GetAnalyticsDto): Promise<PlotDto> {
+  async getAnalytics(
+    @Query() dto: GetAnalyticsDto,
+    @Res() res: Response,
+  ): Promise<void> {
     const cmd = new GetAnalyticsCmd(dto.metric, dto.id);
     const plot = await this.getAnalyticsUseCase.getAnalytics(cmd);
-    return PlotDto.fromDomain(plot);
+
+    res.setHeader('Cache-Control', 'public, max-age=86400'); // 24 ore
+    res.json(PlotDto.fromDomain(plot));
   }
 }

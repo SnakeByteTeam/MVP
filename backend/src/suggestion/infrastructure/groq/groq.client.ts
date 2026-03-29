024a71f744f780c44af523cdfcb22195f9c37dee
val <valeriabaleanu@gmail.com>
2026-03-29 15:36:54 +0200

    Modifica commenti GroqClient

--- backend/src/suggestion/infrastructure/groq/groq.client.ts
+++ backend/src/suggestion/infrastructure/groq/groq.client.ts
@@ -2,11 +2,6 @@ import { GetSuggestionCmd } from 'src/suggestion/application/commands/get-sugges
 import { GroqSuggestionResult } from '../dtos/groq-suggestion-result.dto';
 
 export interface GroqClient {
-  /**
-   * @param current  - dati del grafico corrente da analizzare
-   * @param baseline - dati di riferimento "normali" con cui confrontare
-   * @returns        - testo grezzo della suggestion generata da Gemini
-   */
   generateSuggestion(
     current: GetSuggestionCmd,
     baseline: GetSuggestionCmd,

a27fc522df6b9bbec37c00deeff7a8ddd9bf669f
Francesco Pasqual <francesco.pasqual17@gmail.com>
2026-03-30 11:19:04 +0200

    rimossa cartella inutile

--- wards/infrastructure/dtos/out/add-plant-to-ward-res-dto/add-plant-to-ward-res-dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AddPlantToWardResDto } from './add-plant-to-ward-res-dto';
-
-describe('AddPlantToWardResDto', () => {
-  it('should be defined', () => {
-    expect(new AddPlantToWardResDto()).toBeDefined();
-  });
-});

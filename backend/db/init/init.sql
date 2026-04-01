e59b461f658131eaab1165c7a6d547be8e0ee708
Filippo Venzo <venzofilippo@gmail.com>
2026-04-01 15:16:21 +0200

    aggiunta parte di timeseries

--- backend/db/init/init.sql
+++ backend/db/init/init.sql
@@ -23,3 +23,20 @@ CREATE TABLE plant (
     ward_id INTEGER REFERENCES ward(id) ON DELETE SET NULL
 );
 
+CREATE TABLE datapoint_history (
+    timestamp    TIMESTAMPTZ  NOT NULL,
+    datapoint_id VARCHAR(128)  NOT NULL,
+    value        VARCHAR(50)  NOT NULL,
+    PRIMARY KEY (timestamp, datapoint_id)
+);
+
+SELECT create_hypertable('datapoint_history', 'timestamp');
+
+CREATE INDEX idx_datapoint_history_datapoint_id
+    ON datapoint_history (datapoint_id, timestamp DESC);
+
+ALTER TABLE datapoint_history SET (
+    timescaledb.compress,
+    timescaledb.compress_segmentby = 'datapoint_id'
+);
+
--- backend/src/device/infrastructure/persistence/device-repository-impl.ts
+++ backend/src/device/infrastructure/persistence/device-repository-impl.ts
@@ -4,10 +4,14 @@ import { PG_POOL } from 'src/database/database.module';
 import { FindDeviceByIdRepoPort } from 'src/device/application/repository/find-device-by-id.repository';
 import { FindDeviceByPlantIdRepoPort } from 'src/device/application/repository/find-device-by-plant-id.repository';
 import { DeviceEntity } from './entities/device.entity';
+import { IngestTimeseriesRepoPort } from 'src/device/application/repository/ingest-timeseries.repository';
 
 @Injectable()
 export class DeviceRepositoryImpl
-  implements FindDeviceByIdRepoPort, FindDeviceByPlantIdRepoPort
+  implements
+    FindDeviceByIdRepoPort,
+    FindDeviceByPlantIdRepoPort,
+    IngestTimeseriesRepoPort
 {
   constructor(@Inject(PG_POOL) private readonly pool: Pool) {}
 
@@ -50,6 +54,27 @@ export class DeviceRepositoryImpl
 
       const devices: DeviceEntity[] = rows.map((row) => row.device);
       return devices;
+    } catch (err) {
+      throw new Error(`Database error: ${err.message}`);
+    } finally {
+      client.release();
+    }
+  }
+  async ingestTimeseries(
+    datapointId: string,
+    value: string,
+    timestamp: string,
+  ): Promise<boolean> {
+    const client = await this.pool.connect();
+    try {
+      const result = await client.query(
+        `INSERT INTO datapoint_history (timestamp, datapoint_id, value)
+       VALUES ($1::TIMESTAMPTZ, $2::VARCHAR(128), $3::VARCHAR(50))
+       ON CONFLICT (timestamp, datapoint_id) DO UPDATE SET value = EXCLUDED.value`,
+        [timestamp, datapointId, value],
+      );
+
+      return result.rows.length > 0;
     } catch (err) {
       throw new Error(`Database error: ${err.message}`);
     } finally {

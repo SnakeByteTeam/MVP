55449c9cfa3c3fb3bfe9895ccdc70800bfca7dde
val <valeriabaleanu@gmail.com>
2026-04-05 12:57:52 +0200

    impementazione breadcrumb

--- frontend/src/app/features/main-layout/components/topbar/topbar.component.ts
+++ frontend/src/app/features/main-layout/components/topbar/topbar.component.ts
@@ -1,10 +1,14 @@
 import { Component,EventEmitter, Input, Output } from '@angular/core';
 import { UserInfo } from '../../../../core/models/user-info.model';
 import { UserRole } from '../../../../core/models/user-role.enum';
+import { AsyncPipe } from '@angular/common';
+import { Breadcrumb, BreadcrumbService } from '../../../../core/services/breadcrumb.service';
+import { Observable } from 'rxjs';
 
 @Component({ 
     selector: 'app-topbar', 
-    standalone: true, 
+    standalone: true,
+    imports:[AsyncPipe],
     templateUrl: './topbar.component.html',
     styleUrl: './topbar.component.css'})
 export class TopbarComponent {
@@ -13,6 +17,11 @@ export class TopbarComponent {
     @Output() profileClicked = new EventEmitter<void>();
     @Output() logoutClicked = new EventEmitter<void>();
     @Output() hamburgerClicked = new EventEmitter<void>();
+    breadcrumbs$: Observable<Breadcrumb[]>;
+
+    constructor(private readonly breadcrumbService: BreadcrumbService) {
+        this.breadcrumbs$ = this.breadcrumbService.breadcrumbs$;
+    }
 
     public onProfileClick(): void {
         if (!this.canOpenProfile) {

fe1e1c82dc2d510a4202f70aaada55628970e2e6
Francesco Pasqual <francesco.pasqual17@gmail.com>
2026-04-05 11:20:35 +0200

    aggiornato docker compose, corretto bug user guard

--- backend/src/guard/user/user.guard.ts
+++ backend/src/guard/user/user.guard.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class UserGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(private readonly jwtService: JwtService) { }
 
   canActivate(
     context: ExecutionContext,
@@ -33,6 +33,7 @@ export class UserGuard implements CanActivate {
       });
       if (payload) {
         req.user = payload;
+        return true;
       }
       throw new UnauthorizedException(
         'You are not allowed to access this resource',

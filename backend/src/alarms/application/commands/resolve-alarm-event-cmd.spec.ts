0ca54085b3099458beea4569877d0d99e3157452
Filippo Venzo <venzofilippo@gmail.com>
2026-04-06 09:39:34 +0200

    tolto ultimo head marker

--- backend/src/alarms/application/commands/resolve-alarm-event-cmd.spec.ts
+++ backend/src/alarms/application/commands/resolve-alarm-event-cmd.spec.ts
@@ -2,10 +2,6 @@ import { ResolveAlarmEventCmd } from './resolve-alarm-event-cmd';
 
 describe('ResolveAlarmEventCmd', () => {
   it('should be defined', () => {
-<<<<<<< HEAD
-    expect(new ResolveAlarmEventCmd('ALARM_ID', 1)).toBeDefined();
-=======
     expect(new ResolveAlarmEventCmd('', 1)).toBeDefined();
->>>>>>> prova_integrazione
   });
 });

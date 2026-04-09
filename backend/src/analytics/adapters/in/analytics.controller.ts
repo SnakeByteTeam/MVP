2cf68a66bd550f860bad69bea8a990e660b6c161
Filippo Venzo <venzofilippo@gmail.com>
2026-04-09 21:23:37 +0200

    aggiunta test

--- backend/src/analytics/adapters/in/analytics.controller.ts
+++ backend/src/analytics/adapters/in/analytics.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Inject, Get, Query, Param, UseGuards } from '@nestjs/common';
+import { Controller, Inject, Get, Query, Param } from '@nestjs/common';
 import { GetAnalyticsCmd } from '../../application/commands/get-analytics.cmd';
 import { GetAnalyticsUseCase } from '../../application/ports/in/get-analytics.usecase';
 import { GetAnalyticsDto } from '../../infrastructure/dtos/get-analytics.dto';
@@ -9,8 +9,6 @@ import {
   ApiOperation,
   ApiParam,
 } from '@nestjs/swagger';
-import { AdminGuard } from 'src/guard/admin/admin.guard';
-import { UserGuard } from 'src/guard/user/user.guard';
 
 
 @Controller('analytics')
@@ -20,7 +18,6 @@ export class AnalyticsController {
     private readonly getAnalyticsUseCase: GetAnalyticsUseCase,
   ) {}
 
-  @UseGuards(UserGuard, AdminGuard)
   @Get('/:plantId')
   @ApiOperation({
     summary: 'Get all analytics by Plant ID',

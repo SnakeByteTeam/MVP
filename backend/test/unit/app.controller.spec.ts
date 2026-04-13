479b34aeae09412a56f7dddb997aefa9be4aef52
Filippo Venzo <venzofilippo@gmail.com>
2026-04-13 08:43:26 +0200

    tolto app.controller.ts

--- backend/test/unit/app.controller.spec.ts
+++
@@ -1,9 +0,0 @@
-import { AppController } from 'src/app.controller';
-
-describe('AppController', () => {
-  it('getHello restituisce il testo atteso', () => {
-    const controller = new AppController();
-
-    expect(controller.getHello()).toBe('Hello World!');
-  });
-});
